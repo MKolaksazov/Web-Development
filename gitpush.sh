01aef199cc696de30b9d41375c5d9f23db03682f
root <m.kolaksazov@gmail.com>
2023-11-22 11:22:27 +0200

    Initial commit

--- gitpush.sh
+++ gitpush.sh
@@ -5,6 +5,10 @@ marko1985
 
 sudo git commit -m "Initial commit"
 sudo git pull --rebase origin master
+
+MKolaksazov
+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
+
 sudo git push -u origin master
 MKolaksazov
 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
