84a953760779baf0be7b11a2edd3b10b052b985b
root <m.kolaksazov@gmail.com>
2023-11-22 11:19:09 +0200

    Initial commit

---
+++ gitpush.sh
@@ -0,0 +1,7 @@
+sudo cd /
+sudo git add --all
+sudo git commit -m "Initial commit"
+sudo git pull --rebase origin master
+sudo git push -u origin master
+MKolaksazov
+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
